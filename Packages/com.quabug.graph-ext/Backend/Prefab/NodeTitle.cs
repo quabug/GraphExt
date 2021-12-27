522d6b2b759b2f42ae2804cf19ef525ea7b7942b
quabug <quabug@gmail.com>
2021-12-28 00:50:10 +0800

    custom node title.

---
+++ Packages/com.quabug.graph-ext/Backend/Prefab/NodeTitle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GraphExt
+{
+    public class NodeTitle : MonoBehaviour
+    {
+        public enum TitleType { Hidden, GameObjectName, NodeTitleAttribute, CustomTitle }
+        public TitleType Type;
+        public string CustomTitle;
+    }
+}
