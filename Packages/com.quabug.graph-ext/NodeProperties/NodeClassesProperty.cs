6c173aefb480b3571a613694a99e20753645f9e4
quabug <quabug@gmail.com>
2022-01-04 00:40:04 +0800

    additional classes for node by node property

---
+++ Packages/com.quabug.graph-ext/NodeProperties/NodeClassesProperty.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace GraphExt.Editor
+{
+    public class NodeClassesProperty : INodeProperty
+    {
+        public int Order => 0;
+        public string[] AdditionalClasses;
+
+        public class ViewFactory : NodePropertyViewFactory<NodeClassesProperty>
+        {
+            protected override VisualElement Create(Node node, NodeClassesProperty property, INodePropertyViewFactory factory)
+            {
+                foreach (var @class in property.AdditionalClasses) node.AddToClassList(@class);
+                return null;
+            }
+        }
+    }
+}
