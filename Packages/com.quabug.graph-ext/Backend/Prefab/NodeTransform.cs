fd0b16904d274935f54b8466f10d8ac6d7e24dbf
quabug <quabug@gmail.com>
2022-01-04 15:34:31 +0800

    action to reorder node in hierarchy by its position

---
+++ Packages/com.quabug.graph-ext/Backend/Prefab/NodeTransform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace GraphExt
+{
+    public static class NodeTransform
+    {
+        public static Action<Transform> ReorderChildrenTransformAction<TKey>(Func<INodeComponent, TKey> order)
+        {
+            var isTransforming = false;
+            return parent =>
+            {
+                if (isTransforming) return;
+                isTransforming = true;
+                foreach (var child in parent.GetComponentsInChildren<INodeComponent>().OrderBy(order))
+                {
+                    ((Component)child).transform.SetAsLastSibling();
+                }
+                isTransforming = false;
+            };
+        }
+    }
+}
