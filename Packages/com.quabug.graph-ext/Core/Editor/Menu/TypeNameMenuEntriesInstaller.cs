201bf0a927d00a514497b7aaa4e65fca85e9e84d
quabug <quabug@gmail.com>
2022-01-24 20:20:45 +0800

    menu installers for multiple entries.

---
+++ Packages/com.quabug.graph-ext/Core/Editor/Menu/TypeNameMenuEntriesInstaller.cs
@@ -0,0 +1,15 @@
+﻿using OneShot;
+
+namespace GraphExt.Editor
+{
+    public class TypeNameMenuEntriesInstaller : IMenuEntryInstaller
+    {
+        [SerializedType(typeof(IMenuEntry), Nullable = false, InstantializableType = true, RenamePatter = @"\w*\.||")]
+        public string[] MenuEntries;
+
+        public void Install(Container container)
+        {
+            container.RegisterTypeNameArraySingleton<IMenuEntry>(MenuEntries);
+        }
+    }
+}
