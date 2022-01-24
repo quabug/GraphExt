cf21b406b30eb57603f2dceb8a60057bf8adc7e2
quabug <quabug@gmail.com>
2022-01-24 20:28:25 +0800

    visual node menu entry

---
+++ Assets/Sample/ExpressionTree/VisualNodeMenuEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GraphExt;
+using GraphExt.Editor;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class VisualNodeMenuEntry : NodeMenuEntry<IVisualNode>
+{
+    public VisualNodeMenuEntry([NotNull] GraphRuntime<IVisualNode> graphRuntime, [NotNull] IDictionary<NodeId, Vector2> positions) : base(graphRuntime, positions)
+    {
+    }
+}
