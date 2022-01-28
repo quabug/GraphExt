dce39ab906b951d2249d1c35795035f8b00d192e
quabug <quabug@gmail.com>
2022-01-28 14:15:26 +0800

    title label view for title properties (#36)
    * add title label for all title properties
    
    * update version

---
+++ Packages/com.quabug.graph-ext/Core/Editor/NodeProperties/EventTitleProperty.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace GraphExt.Editor
+{
+    public class EventTitleProperty : INodeProperty
+    {
+        public int Order => 0;
+        private string _title;
+        public string Title
+        {
+            private get => _title;
+            set
+            {
+                _title = value;
+                TitleChanged?.Invoke(_title);
+            }
+        }
+
+        private event Action<string> TitleChanged;
+
+        class View : TitleLabel
+        {
+            public View(EventTitleProperty property) : base(property.Title)
+            {
+                property.TitleChanged += title => text = title;
+            }
+        }
+
+        public class ViewFactory : SingleNodePropertyViewFactory<EventTitleProperty>
+        {
+            protected override VisualElement CreateView(Node node, EventTitleProperty property, INodePropertyViewFactory factory)
+            {
+                return new View(property);
+            }
+        }
+    }
+}
---
+++ Packages/com.quabug.graph-ext/Core/Editor/NodeProperties/SerializedTitleProperty.cs
@@ -0,0 +1,43 @@
+using System.Reflection;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+
+namespace GraphExt.Editor
+{
+    public class SerializedTitleProperty : INodeProperty
+    {
+        public int Order => 0;
+        public SerializedProperty Property;
+
+        public class Factory : INodePropertyFactory
+        {
+            public INodeProperty Create(MemberInfo memberInfo, object nodeObj, NodeId nodeId, SerializedProperty fieldProperty)
+            {
+                Assert.IsNotNull(fieldProperty);
+                Assert.AreEqual(SerializedPropertyType.String, fieldProperty.propertyType);
+                return new SerializedTitleProperty { Property = fieldProperty };
+            }
+        }
+
+        private class View : TitleLabel
+        {
+            public View(SerializedProperty property) : base(property.stringValue)
+            {
+                Assert.AreEqual(SerializedPropertyType.String, property.propertyType);
+                this.TrackPropertyValue(property, p => text = p.stringValue);
+            }
+        }
+
+        [UsedImplicitly]
+        public class ViewFactory : SingleNodePropertyViewFactory<SerializedTitleProperty>
+        {
+            protected override VisualElement CreateView(Node node, SerializedTitleProperty property, INodePropertyViewFactory factory)
+            {
+                return new View(property.Property);
+            }
+        }
+    }
+}
